81e1fc18fb463a3741263f5b85325e1edcf84042
Luda Zhao <94luda@gmail.com>
2016-12-26 17:58:24 -0800

    added basic flight info

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+from __future__ import print_function
 import os
 import sys
 import json
 
 import requests
+import string
 from flask import Flask, request
+
+import feedparser
 
 app = Flask(__name__)
 
@@ -38,8 +42,19 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-
-                    send_message(sender_id, "got it, thanks!")
+                    if message_text == "flights":
+                        res = u""
+                        try:
+                            d = feedparser.parse('http://feeds.feedburner.com/TheFlightDeal?format=xml')
+                            for entry in d['entries']:
+                                if 'san francisco' in entry['title'].lower() or 'los angeles' in entry['title'].lower():
+                                    res = "%s\n%s\n%s\n" % (entry['title'], entry['feedburner_origlink'], entry['published'])
+                            if res == "":
+                                res = "<No results>"
+                        except Exception as e:
+                            res = "An error has occurred in parsing."
+                        #log(res)
+                        send_text(sender_id, res)
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
@@ -53,10 +68,16 @@ def webhook():
     return "ok", 200
 
 
-def send_message(recipient_id, message_text):
+def send_text(recipient_id, message_text):
+    message_data = {'text': message_text}
+    #log("sending text message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+    post_message(recipient_id, message_data)
 
-    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+# TODO: send detailed itin info when needed
+def send_detailed_info(recipient_id, message_arg):
+    pass
 
+def post_message(recipient_id, message_data):
     params = {
         "access_token": os.environ["PAGE_ACCESS_TOKEN"]
     }
@@ -67,9 +88,7 @@ def send_message(recipient_id, message_text):
         "recipient": {
             "id": recipient_id
         },
-        "message": {
-            "text": message_text
-        }
+        "message": message_data
     })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
@@ -79,7 +98,7 @@ def send_message(recipient_id, message_text):
 
 
 def log(message):  # simple wrapper for logging to stdout on heroku
-    print str(message)
+    print(message)
     sys.stdout.flush()
 
 
